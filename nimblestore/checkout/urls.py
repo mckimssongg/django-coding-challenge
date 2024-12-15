2ec3f4fce7bcfd34bddd1d602a55f60b146d31e2
mckimssongg <soymckimsson286@gmail.com>
2024-12-15 00:23:21 -0600

    Añadir django-extensions a INSTALLED_APPS y ajustar rutas en urls.py

--- nimblestore/checkout/urls.py
+++ nimblestore/checkout/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from . import views
 
 router = DefaultRouter()
-router.register(r'api/products', views.ProductListView, basename='product')
+router.register(r"products", views.ProductListView, basename="product")
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("api", views.IndexView.as_view(), name="index"),
     path("api/order/", views.OrderView.as_view(), name="order"),
-    path("", include(router.urls)),
+    path("api/", include(router.urls)),
 ]
--- nimblestore/nimblestore/settings.py
+++ nimblestore/nimblestore/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "checkout",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
